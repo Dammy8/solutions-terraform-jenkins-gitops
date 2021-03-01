adc05672076411187a995930559660dd2848ace4
damodhar@cozysystems.net <damodhar@cozysystems.net>
2021-02-28 20:26:24 -0500

    upgrading helm jenkins from 1.9.18 to 2.17.0

--- jenkins-gke/tf-gke/main.tf
+++ jenkins-gke/tf-gke/main.tf
@@ -190,7 +190,7 @@ resource "helm_release" "jenkins" {
   name       = "jenkins"
   repository = "https://kubernetes-charts.storage.googleapis.com"
   chart      = "jenkins"
-  version    = "1.9.18"
+  version    = "2.17.0"
   timeout    = 1200
 
   values = [data.local_file.helm_chart_values.content]
